142423635f1723f336e830001f85c3f5745adb08
DESKTOP-8NC8JNS\Joao <joaoliveira1990@gmail.com>
2022-07-12 03:10:49 +0100

    changed colors and changed profile picture

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,8 @@
 import './index.scss'
 import { Link, NavLink } from 'react-router-dom'
-import LogoS from '../../assets/images/logo-s.png'
-import LogoSubtitle from '../../assets/images/logo_sub.png'
+/* import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png' */
+import ProfilePicture from '../../assets/images/lady.jpg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 import {
@@ -13,8 +14,9 @@ import {
 const Sidebar = () => (
   <div className="nav-bar">
     <Link className="logo" to="/">
-      <img src={LogoS} alt="logo" />
-      <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
+      <img src={ProfilePicture} alt="lady" title='lady!'/>
+      {/* <img src={LogoS} alt="logo" />
+      <img className="sub-logo" src={LogoSubtitle} alt="slobodan" /> */}
     </Link>
     <nav>
       <NavLink exact="true" activeclassname="active" to="/">
